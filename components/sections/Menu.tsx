ceaae20aa1214be384f245576293a38ec17aa755
IGUIDER-AMINE <amineiguider2017@gmail.com>
2023-07-09 19:46:20 +0200

    finish sidebar , navbar, banner

--- components/sections/Menu.tsx
+++ components/sections/Menu.tsx
@@ -2,9 +2,14 @@ import React from "react";
 
 const Menu = () => {
   return (
-    <div id="Menu" className="space-y-8 container mx-auto px-4 lg:px-0">
-      Menu
-    </div>
+    <section id="Menu" className="container mx-auto px-4 lg:px-0">
+      <div className="space-y-8 bg-gradient-to-r from-[#34393E] via-[#2F3338] to-[#2B2F33] min-h-[500px] ">
+        Menu
+      </div>
+      <div className="space-y-8 bg-gradient-to-r from-[#F0F0F0] via-[#F0F0F0] to-[#E5E5E5] min-h-[500px] ">
+        Menu
+      </div>
+    </section>
   );
 };
 
