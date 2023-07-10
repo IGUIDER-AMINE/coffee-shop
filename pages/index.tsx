6fc8548a95cdf1377513850fc084c8c2ef642057
IGUIDER-AMINE <amineiguider2017@gmail.com>
2023-07-10 08:41:33 +0200

    push

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import dynamic from "next/dynamic";
+import Layout from "@/components/layout/Layout";
 
-import Layout from "@/components/layout/layout";
 const Banner = dynamic(() => import("@/components/sections/Banner"));
 const Menu = dynamic(() => import("@/components/sections/Menu"));
 const Facility = dynamic(() => import("@/components/sections/Facility"));
