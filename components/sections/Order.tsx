e29024b488a76189d74ad45292f7f3aa15f40149
IGUIDER-AMINE <amineiguider2017@gmail.com>
2023-07-12 11:30:55 +0200

    finish order

--- components/sections/Order.tsx
+++ components/sections/Order.tsx
@@ -4,20 +4,20 @@ const Order = () => {
   return (
     <section
       id="Order"
-      className="container mx-auto px-4 lg:px-24 grid gap-x-8 grid-cols-12 items-center py-36"
+      className="container mx-auto px-4 lg:px-24 gap-y-8 gap-x-4 flex flex-wrap py-20 items-center justify-between"
     >
-      <h1 className="text-[#2B2F33] col-span-3 text-4xl lg:text-5xl xl:text-5xl font-medium font-titleFont">
+      <h1 className="text-[#2B2F33] text-4xl lg:text-5xl xl:text-5xl font-medium font-titleFont">
         Take Your <br /> Coffe Now
       </h1>
-      <div className="col-span-7">
-        <p className="text-[#787E85] max-w-[80%] mx-auto font-medium">
+      <div className="md:w-[40%]">
+        <p className="text-[#787E85] font-medium">
           Don&apos;t let your coffee cold, let&apos;s go to{" "}
           <span className="font-semibold">Couffeeee</span> and get your coffe
           and boost your day now.
         </p>
       </div>
-      <div className="col-span-2  h-auto">
-        <button className="text-white border-[3px] border-l-[#484956] border-t-[#909299] w-full shadow border-b-[#484956] bg-[#1F2131] py-2 px-3">
+      <div>
+        <button className="text-white border-[3px] border-l-[#484956] border-t-[#909299] shadow border-b-[#484956] bg-[#1F2131] py-2 px-3">
           Our Location
         </button>
       </div>
